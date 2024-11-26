a89093f9e4c503f79bf911f303af4f53f83ad3ba
guiusa <guiusepeoneda@gmail.com>
2024-11-26 17:44:19 -0300

    adicionando bases, não funciona

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include <z3++.h>
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <iostream>
 using namespace std;
+using namespace z3;
 
 // using int& to avoid copying the values into memory
 vector<vector<int>> readMatrix(int& n, int& m) {
@@ -35,6 +38,63 @@ int main() {
     cout << "Dimensões: " << n << " x " << m << endl;
     cout << "Matriz:" << endl;
     printMatrix(matrix);
+
+    context c ;
+    solver s(c) ;
+
+    vector<vector<expr>> cell(n, vector<expr>(m, c.bool_val(false)));
+    for(int i = 0; i<n; i++)
+        for(int j = 0; j<m; j++)
+            cell[i][j] = c.bool_const(("p_" + to_string(i) + "_" + to_string(j)).c_str()) ;
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            expr neighbour_sum = c.int_val(0) ;
+
+            for(int di = -1; di<=1; di++) {
+                for(int dj = -1; dj<=1; dj++){
+                    if(di == 0 && dj == 0) continue ;
+
+                    int ni = i+di ;
+                    int nj = j+dj ;
+
+                    if(ni >= 0 && ni < n && nj >= 0 && nj < m) {
+                        neighbour_sum = neighbour_sum + ite(cell[ni][nj], c.int_val(1), c.int_val(0)) ;
+                    }
+                }
+            }
+
+            if (matrix[i][j] == 1) {
+                s.add((cell[i][j] && (neighbour_sum == 2 || neighbour_sum == 3)) ||
+                        (!cell[i][j] && neighbour_sum == 3)) ;
+            } else {
+                s.add((!cell[i][j] && neighbour_sum != 3) ||
+                        (cell[i][j] && (neighbour_sum < 2 || neighbour_sum > 3))) ;
+            }
+        }
+    }
+
+    optimize opt(c) ;
+
+    expr total_live_cells = c.int_val(0) ;
+    for(int i = 0; i<n; i++) {
+        for(int j = 0 ; j<m; j++){
+            total_live_cells = total_live_cells + ite(cell[i][j], c.int_val(1), c.int_val(0)) ;
+        }
+    }
+
+    opt.minimize(total_live_cells) ;
+
+    if(opt.check() == sat) {
+        model mdl = opt.get_model() ;
+
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                cout << (mdl.eval(cell[i][j]).is_true() ? "1 " : "0 ") ;
+            }
+            cout << "\n" ;
+        }
+    }
 
     return 0;
 }
